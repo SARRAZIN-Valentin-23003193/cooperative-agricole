1fa6920ea069154485077bb83c95825262f6f8a5
gallo <loic.gallou@etu.univ-amu.fr>
2025-04-02 18:48:03 +0200

    potentiel preparation pour les users

--- IHM/main/controllers/MainController.php
+++ IHM/main/controllers/MainController.php
@@ -1,26 +1,55 @@
 <?php
 namespace controllers;
 
-use gui\Layout;
-use gui\ViewHome;
-use gui\ViewLogin;
+use data\ApiUser;
+use domain\User;
 
-include_once 'gui/ViewHome.php'; // Inclure le fichier de la classe ViewHome
-include_once 'gui/ViewLogin.php'; // Inclure le fichier de la classe ViewHome
+class MainController
+{
+private $apiUser;
 
-class MainController {
+public function __construct()
+{
+$this->apiUser = new ApiUser();
+}
 
-    public function homePage(): void
-    {
-        $layout = new Layout("gui/layout.html");
-        $view = new ViewHome($layout);
-        $view->display();
-    }
+// Affichage de la page d'accueil
+public function homePage()
+{
+$layout = new \gui\Layout();
+$view = new \gui\ViewHome($layout);
+$view->display();
+}
 
-    public function loginPage(): void
-    {
-        $layout = new Layout("gui/layout.html");
-        $view = new ViewLogin($layout);
-        $view->display();
-    }
+// Affichage du formulaire de connexion et traitement de la connexion
+public function loginPage()
+{
+// Vérification de la soumission du formulaire de connexion
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Récupération des données du formulaire
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+// Appel à l'API pour authentifier l'utilisateur
+$user = $this->apiUser->authenticateUser($login, $password);
+
+if ($user) {
+// Si l'utilisateur est authentifié, on stocke son ID dans la session
+$_SESSION['user_id'] = $user->getId();
+$_SESSION['user_name'] = $user->getName();
+
+// Rediriger vers la page d'accueil ou autre page (ici on redirige vers la page d'accueil)
+header('Location: index.php?action=home');
+exit();
+} else {
+// Si l'authentification échoue, on peut afficher un message d'erreur
+$errorMessage = "Identifiants invalides.";
+}
+}
+
+// Affichage du formulaire de connexion
+$layout = new \gui\Layout();
+$view = new \gui\ViewLogin($layout, isset($errorMessage) ? $errorMessage : '');
+$view->display();
+}
 }
--- IHM/main/gui/ViewLogin.php
+++ IHM/main/gui/ViewLogin.php
@@ -1,33 +1,38 @@
 <?php
-
 namespace gui;
 
-require_once 'Layout.php';
+class ViewLogin
+{
+private $layout;
+private $errorMessage;
 
-class ViewLogin {
-    private $layout;
-
-    public function __construct($layout) {
-        $this->layout = $layout;
-    }
-
-    public function display() {
-        // Création du formulaire de connexion
-        $content = "
-        <h2>Connexion</h2>
-        <form action='index.php' method='POST'>
-            <label for='login'>Nom d'utilisateur :</label><br>
-            <input type='text' id='login' name='login' required><br><br>
-
-            <label for='password'>Mot de passe :</label><br>
-            <input type='password' id='password' name='password' required><br><br>
-
-            <input type='submit' value='Se connecter'>
-        </form>
-        ";
-
-        // Affichage du contenu dans la structure de la page (via Layout)
-        $this->layout->render($content);
-    }
+public function __construct($layout, $errorMessage = '')
+{
+$this->layout = $layout;
+$this->errorMessage = $errorMessage;
 }
 
+public function display()
+{
+// Formulaire de connexion
+$content = "
+<h2>Connexion</h2>
+<form action='index.php?action=login' method='POST'>
+    <label for='login'>Nom d'utilisateur :</label><br>
+    <input type='text' id='login' name='login' required><br><br>
+
+    <label for='password'>Mot de passe :</label><br>
+    <input type='password' id='password' name='password' required><br><br>
+
+    <input type='submit' value='Se connecter'>
+</form>";
+
+// Si un message d'erreur existe, on l'affiche
+if (!empty($this->errorMessage)) {
+$content .= "<p style='color: red;'>{$this->errorMessage}</p>";
+}
+
+// Affichage final avec le layout
+$this->layout->render($content);
+}
+}
